f1235833b43de499ff0f02ef78a555e955d9ebe7
Arthur Attout <arthur.attout@outlook.com>
2019-05-17 00:56:22 +0200

    Auto retrieve reports

--- server.py
+++ server.py
@@ -28,15 +28,15 @@ mydb = mysql.connector.connect(
 	database="biomass_database"
 )
 
-def get_biomass_name_from_class(class_number):
+def get_biomass_info_from_class(class_number):
 	cursor = mydb.cursor()
 	query = '''	
-		select name from biomass
+		select id, name from biomass
 		where class_ML = {}
 	'''
 	cursor.execute(query.format(class_number))
 	result = cursor.fetchall()
-	return [x[0] for x in result][0]
+	return result[0]
 	
 	
 def get_valorizations_for_biomass(class_number):
@@ -93,9 +93,9 @@ def identifyHandler():
 	
 	if(max(JSON_object['predictions']) > 0.95):
 		# If certitude > 95%, return object as-is
-		biomass_id = JSON_object['likely_class']
-		print("Getting biomass name for {}".format(biomass_id))
-		biomass_name = get_biomass_name_from_class(biomass_id)
+		biomass_ML_class = JSON_object['likely_class']
+		print("Getting biomass name for {}".format(biomass_ML_class))
+		biomass_id,biomass_name = get_biomass_info_from_class(biomass_ML_class)
 		certitude =  max(JSON_object['predictions'])
 		response_client = {
 			"result" : "OK",
@@ -107,11 +107,12 @@ def identifyHandler():
 		new_id = add_to_history(biomass_id,url_host,certitude)
 		payload_dashboard = {
 			"type":"NEW_HISTORY",
-			"element":{
+			"elem":{
 				"id":new_id,
 				"date": str(datetime.datetime.now()),
 				"name": biomass_name,
-				"certitude": certitude
+				"certitude": certitude,
+				"path":url_host
 			}
 		}
 		ws = create_connection("ws://192.168.1.85:8080/")
@@ -184,6 +185,68 @@ def geolocationHandler():
 			"result":"BAD_CERTITUDE"
 		}
 		return json.dumps(response)
+	
+@app.route('/new_report', methods = ['POST'])
+def reportHandler():
+
+	content = request.get_json()
+	submission_date = content['submission_date']
+	latitude = content['latitude']
+	longitude = content['longitude']
+	comment = content['comment']
+	images = content['images']
+	
+	print("Inserting new report")
+	cursor = mydb.cursor()
+	query = '''
+		INSERT INTO report (status, submission_date, latitude, longitude, comment, FK_Researcher) 
+		VALUES ('{}','{}','{}','{}','{}','{}');
+	'''
+	cursor.execute(query.format(1,submission_date,latitude,longitude,comment,1))
+	new_report_id = cursor.lastrowid
+	print("New report : {}".format(new_report_id))
+	mydb.commit()
+	
+	for image_path in images:
+	
+		print("Inserting in report_image")
+		cursor = mydb.cursor()
+		query = '''
+			INSERT INTO report_image (path) 
+			VALUES ('{}');
+		'''
+		cursor.execute(query.format(image_path))
+		new_image_id = cursor.lastrowid
+		print("New image : {}".format(new_image_id))
+		mydb.commit()
+		
+		print("Inserting in report_to_image")
+		cursor = mydb.cursor()
+		query = '''
+			INSERT INTO report_to_image (FK_Report,FK_Image) 
+			VALUES ({},{});
+		'''
+		print("Values : {},{}".format(new_report_id, new_image_id))
+		cursor.execute(query.format(new_report_id,new_image_id))
+		mydb.commit()
+	
+	payload_dashboard = {
+		"type":"NEW_REPORT",
+		"new_report":{
+			"id": new_report_id,
+			"submission_date": submission_date,
+			"images": images,
+			"latitude": latitude,
+			"longitude": longitude,
+			"comment": comment,
+			"status":1
+		}
+	}
+	ws = create_connection("ws://192.168.1.85:8080/")
+	ws.send(json.dumps(payload_dashboard))
+	ws.close()
+	
+	return "OK"
 	
 if __name__ == '__main__':
     app.run(host = '0.0.0.0',port=5000)
